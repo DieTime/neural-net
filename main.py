542d4c694f11f41beceb3faf890e57d432fa88a6
DieTime <dietimemovedme@gmail.com>
2020-03-11 22:43:03 +0300

    Espen Sande Larsen - Refactor

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from network import Network
 
 a = Network([
-    Network.input(neurons=3),
+    Network.input(neurons=2),
     Network.hidden(neurons=3, activation='tanh'),
     Network.hidden(neurons=3, activation='tanh'),
-    Network.output(neurons=3, activation='leaky_relu')
+    Network.output(neurons=1, activation='sigmoid')
 ])
 
-print(a.prediction([1, 0, 1]))
+data_set = {
+    "input": [[0, 0], [0, 1], [1, 0], [1, 1]],
+    "output": [[0], [1], [1], [0]]
+}
+
+a.train(data_set['input'], data_set['output'], 10000, 0.02, False)
+print(a.prediction([0, 0]))
+print(a.prediction([0, 1]))
+print(a.prediction([1, 0]))
+print(a.prediction([1, 1]))
--- network.py
+++ network.py
@@ -11,16 +11,6 @@ class Network(object):
         # List of weights data
         self.__weights = []
 
-        # Dictionary of activation functions
-        activations = {
-            'linear': np.vectorize(self.linear),
-            'sigmoid': np.vectorize(self.sigmoid),
-            'tanh': np.vectorize(self.tanh),
-            'relu': np.vectorize(self.relu),
-            'leaky_relu': np.vectorize(self.leaky_relu),
-            'softmax': self.softmax
-        }
-
         # Error processing
         if setup[0]['neurons'] < 1:
             raise ValueError('Number of neurons in layer can\'t be less then one!')
@@ -31,17 +21,25 @@ class Network(object):
             # Error processing
             if setup[i]['neurons'] < 1:
                 raise ValueError('Number of neurons in layer can\'t be less then zero!')
-            if not (setup[i]['activation'] in activations):
-                raise ValueError('Invalid activation function name!')
 
             self.__weights.append({
                 'matrix': np.random.random_sample((setup[i - 1]['neurons'], setup[i]['neurons'])),
-                'activation': activations[setup[i]['activation']],
+                'activation': setup[i]['activation'],
                 'bias': random.random()
             })
 
     # Feed forward process
     def prediction(self, input_data):
+        # Dictionary of activation functions
+        activations = {
+            'linear': np.vectorize(self.linear),
+            'sigmoid': np.vectorize(self.sigmoid),
+            'tanh': np.vectorize(self.tanh),
+            'relu': np.vectorize(self.relu),
+            'leaky_relu': np.vectorize(self.leaky_relu),
+            'softmax': self.softmax
+        }
+
         # Clear list of layers
         self.__layers.clear()
 
@@ -53,13 +51,46 @@ class Network(object):
             # Remember layer values
             self.__layers.append(layer)
             # Calculate current layer
-            layer = data['activation'](np.dot(layer, data['matrix']) + data['bias'])
+            layer = activations[data['activation']](np.dot(layer, data['matrix']) + data['bias'])
 
         # Remember layer values
         self.__layers.append(layer)
 
         # Output result
         return layer
+
+    # Back propagation process
+    def back_propagation(self, input_data, output_data, lr):
+        self.prediction(input_data)
+
+        # Dictionary of gradient functions
+        gradients = {
+            'linear': np.vectorize(self.linear2derivative),
+            'sigmoid': np.vectorize(self.sigmoid2derivative),
+            'tanh': np.vectorize(self.tanh2derivative),
+            'relu': np.vectorize(self.relu2derivative),
+            'leaky_relu': np.vectorize(self.leaky_relu2derivative),
+            'softmax': self.softmax2derivative
+        }
+
+        error = np.array(output_data) - self.__layers[-1]
+        for i in reversed(range(len(self.__weights))):
+            gradient = gradients[self.__weights[i]['activation']](self.__layers[i + 1])
+            delta = np.dot(self.__layers[i].T, error * gradient * lr)
+            self.__weights[i]['matrix'] += delta
+            error = np.dot(error, self.__weights[i]['matrix'].T)
+
+        return np.sum(np.square(np.array(output_data) - self.__layers[-1]))
+
+    def train(self, input_data, output_data, epochs, lr, shuffle=False, logging=True):
+        for i in range(epochs):
+            indexes = list(range(len(input_data)))
+            if shuffle:
+                random.shuffle(indexes)
+            for j in indexes:
+                error = self.back_propagation(input_data[j], output_data[j], lr)
+                if logging:
+                    print("Error: " + '{:.5}'.format(error))
 
     # ------------------------ STATIC METHODS --------------------------
 
@@ -85,7 +116,32 @@ class Network(object):
 
     @staticmethod
     def softmax(vector):
-        return np.exp(vector) / np.exp(vector).sum(axis=0)
+        e = np.exp(vector - vector.max())
+        return e / np.sum(e)
+
+    @staticmethod
+    def linear2derivative(x):
+        return 1
+
+    @staticmethod
+    def sigmoid2derivative(x):
+        return x * (1 - x)
+
+    @staticmethod
+    def tanh2derivative(x):
+        return 1 - x ** 2
+
+    @staticmethod
+    def relu2derivative(x):
+        return x >= 0
+
+    @staticmethod
+    def leaky_relu2derivative(x):
+        return 1 if x >= 0 else 0.01
+
+    @staticmethod
+    def softmax2derivative(vector):
+        return vector * (1. - vector)
 
     @staticmethod
     def input(neurons):
